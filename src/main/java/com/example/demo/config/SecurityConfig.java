9a7b3e7ace09f0e4c86b145ddc49d389d818c0c3
gnavia <ginavia12@gmail.com>
2025-06-25 20:03:22 -0400

    configuracion jwt

--- src/main/java/com/example/demo/config/SecurityConfig.java
+++ src/main/java/com/example/demo/config/SecurityConfig.java
@@ -1,6 +1,6 @@
 package com.example.demo.config;
 
-
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpStatus;
@@ -8,33 +8,39 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import com.example.demo.config.security.JwtAuthenticationFilter;
 
 @Configuration
 public class SecurityConfig {
+
+    @Autowired
+    private JwtAuthenticationFilter jwtAuthenticationFilter;
+
     @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
             .csrf(csrf -> csrf.disable())
-            .exceptionHandling(ex-> 
-                ex.accessDeniedHandler((request, response , accessDeniedException)->{
+            .exceptionHandling(ex -> 
+                ex.accessDeniedHandler((request, response, accessDeniedException) -> {
                     response.setStatus(HttpStatus.FORBIDDEN.value());
-                    response.setContentType("aplication/json");
-                    response.getWriter().write("{\"error\":\"Acceso denegado- no tienes permisos\"}");
-                }))
+                    response.setContentType("application/json");
+                    response.getWriter().write("{\"error\":\"Acceso denegado - no tienes permisos\"}");
+                })
+            )
             .authorizeHttpRequests(auth -> auth
-               
-                
                 .requestMatchers("/auth/login").permitAll()
                 .requestMatchers("/auth/hello").authenticated()
-                .anyRequest().authenticated()// necesidad para login 
-            );
+                .anyRequest().authenticated()
+            )
+            .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
 
         return http.build();
-
     }
 
-     @Bean
-     public PasswordEncoder passwordEncoder(){
+    @Bean
+    public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
-     }
+    }
 }
